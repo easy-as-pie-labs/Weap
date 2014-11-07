b11890d7f34cdda9f062628bd66f92da95fe35fc
Jonas Möller <jonas.moeller@web.de>
2014-11-07 03:00:36 +0100

    fixed range bug

--- threaded/Wieferich.py
+++ threaded/Wieferich.py
@@ -23,22 +23,27 @@ class MyThread(Thread):
         self.searchWieferich()
 
     def searchWieferich(self):
-        for i in range(self.rangeStart, self.rangeEnd):
+        i = self.rangeStart + 1
+        while i <= self.rangeEnd:
             if(isWieferich(i)):
-                print "might be wieferich: " + str(i)
+                print self.name + " might have found a wieferich: " + str(i)
             if(i%100000 == 1):
-                print i
-            i+=2
+                print self.name + " currently at: " + str(i)
+            i += 2
 
 if __name__ == '__main__':
     firstThread = MyThread(10**17, 10**18)
-    secondThread = MyThread(10**18, 10**19)
+    firstThread.setName('Thread 1')
+    secondThread = MyThread(10**18,  10**19)
+    secondThread.setName('Thread 2')
     thirdThread = MyThread(10**19, 10**20)
+    thirdThread.setName('Thread 3')
     fourthThread = MyThread(10**20, 10**21)
+    fourthThread.setName('Thread 4')
 
     firstThread.start()
     secondThread.start()
+    '''
     thirdThread.start()
     fourthThread.start()
-
-
+    '''
